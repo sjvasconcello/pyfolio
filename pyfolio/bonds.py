851ec6418cd47a605fb55dcb70dbb5513f0f6a30
sjvasconcello <sjvasconcello@gmail.com>
2024-06-09 03:39:20 -0400

    🍻 Working in coupon tests

--- pyfolio/bonds.py
+++ pyfolio/bonds.py
@@ -1,9 +1,9 @@
 
-from money import Money
+from .money import Money
 from typing import Literal
 
 class Bond:
-    def __init__(self, principal: float, maturity, interest_rate: float, type: Literal['c','d'] = 'd'):      
+    def __init__(self, principal: float, maturity, interest_rate: float, type_m: Literal['c','d'] = 'd'):      
         # Principal amount
         self.principal = Money(principal)
         # Date of maturity
@@ -11,33 +11,36 @@ class Bond:
         # Market interest rate (discounting rate)
         self.interest_rate = interest_rate
     
-        self.type = type
+        self.type_m = type_m
 
     def _principal_amount(self):
         return self.principal._amount
 
 class ZeroCouponBond(Bond):
+    def __init__(self, principal, maturity, interest_rate, type_m: Literal['c','d'] = 'd'):
+        super().__init__(principal, maturity, interest_rate, type_m)
 
     def calculate_price(self):
-        return self.principal.pv(r = self.interest_rate, n = self.maturity, type = self.type)
+        return self.principal.pv(r = self.interest_rate, n = self.maturity, type = self.type_m)
 
     def __repr__(self):
-        return f"ZeroCouponBond({self.principal}, {self.maturity}, {self.interest_rate})"
+        return f"ZeroCouponBond({self._principal_amount}, {self.maturity}, {self.interest_rate})"
       
 class CouponBond(Bond):
-    def __init__(self, rate: float):      
+    def __init__(self, principal, maturity, interest_rate, rate: float, type_m: Literal['c','d'] = 'd'):
+        super().__init__(principal, maturity, interest_rate, type_m) 
         # Rate of the bond
         self.rate = rate
         # Payments
         self.payments = []
 
     def __repr__(self):
-        return f"CouponBondBond({self.principal}, {self.maturity}, {self.interest_rate})"     
+        return f"CouponBondBond({self._principal_amount}, {self.maturity}, {self.interest_rate})"     
     
     def calculate_price(self):
         # Discount the coupon payments
         for t in range(1, self.maturity + 1):
             payment =  Money(self._principal_amount * self.rate)
-            self.payments.append(payment.pv(r = self.interest_rate, n = t, type = self.type))
+            self.payments.append(payment.pv(r = self.interest_rate, n = t, type = self.type_m))
 
-        return sum(self.payments) + self.principal.pv(r = self.interest_rate, n = self.maturity, type = self.type)
+        return sum(self.payments) + self.principal.pv(r = self.interest_rate, n = self.maturity, type = self.type_m)
---
+++ tests/test_bonds.py
@@ -0,0 +1,34 @@
+import unittest
+from pyfolio.bonds import ZeroCouponBond, CouponBond
+
+class TestBond(unittest.TestCase):
+    def test_ZeroCouponBond(self):
+        # Crear un bono cupón cero
+        bond = ZeroCouponBond(1000, 10, 0.05)
+
+        # Test calcular precio
+        self.assertAlmostEqual(bond.calculate_price(), 613.91, delta=0.01)
+
+        # Test representación de cadena
+        self.assertEqual(repr(bond), "ZeroCouponBond(1000, 10, 0.05)")
+
+    def test_CouponBond(self):
+        # Crear un bono cupón
+        bond = CouponBond(1000, 10, 0.05, 0.04)
+
+        # Test calcular precio
+        self.assertAlmostEqual(bond.calculate_price(), 706.67, delta=0.01)
+
+        # Test representación de cadena
+        self.assertEqual(repr(bond), "CouponBond(1000, 10, 0.05, 0.04)")
+
+    def test_invalid_type(self):
+        # Verificar tipo de interés inválido
+        with self.assertRaises(ValueError):
+            ZeroCouponBond(1000, 10, 0.05, 0.04, type_m='x')
+        
+        with self.assertRaises(ValueError):
+            CouponBond(1000, 10, 0.05, 0.04, type_m='x')
+
+if __name__ == '__main__':
+    unittest.main()
